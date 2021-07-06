943c49bae044dad45ba803ce7e2f32bd539f9a5d
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-07-06 13:58:25 +0300

    form

--- src/components/updateForm.js
+++ src/components/updateForm.js
@@ -6,20 +6,16 @@ export class updateForm extends Component {
     return (
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Name" />
+          
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Img url</Form.Label>
+          <Form.Control type="text" placeholder="enter Img" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+    
         <Button variant="primary" type="submit">
           Submit
         </Button>
